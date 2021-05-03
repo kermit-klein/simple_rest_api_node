a7937f550af688531f264b7c25bebd0ca4fce0ce
Ali Erbay <alierbay@Alis-MacBook-Pro.local>
2021-05-03 20:33:40 +0200

    Add express,nodemon,models

---
+++ models/index.js
@@ -0,0 +1,78 @@
+let rocks = {
+  1: {
+    id: "1",
+    name: "Granite",
+    type: "Igneous",
+    img: "https://geology.com/rocks/pictures/granite-coarse-grained-380.jpg",
+    description:
+      "Granite is a light-colored igneous rock with grains large enough to be visible with the unaided eye. It forms from the slow crystallization of magma below Earth's surface.",
+  },
+  2: {
+    id: "2",
+    name: "Basalt",
+    type: "Igneous",
+    img: "https://geology.com/rocks/pictures/basalt-380.jpg",
+    description:
+      "Basalt is a dark-colored, fine-grained, igneous rock composed mainly of plagioclase and pyroxene minerals.",
+  },
+  3: {
+    id: "3",
+    name: "Rhyolite",
+    type: "Igneous",
+    img: "https://geology.com/rocks/pictures/rhyolite-380.jpg",
+    description:
+      "Rhyolite is an extrusive igneous rock with a very high silica content. It is usually pink or gray in color with grains so small that they are difficult to observe without a hand lens.",
+  },
+  4: {
+    id: "4",
+    name: "Marble",
+    type: "Metamorphic",
+    img: "https://geology.com/rocks/pictures/pink-marble.jpg",
+    description:
+      "Marble is a metamorphic rock that forms when limestone is subjected to the heat and pressure of metamorphism. ",
+  },
+  5: {
+    id: "5",
+    name: "Gneiss",
+    type: "Metamorphic",
+    img: "https://geology.com/rocks/pictures/gneiss.jpg",
+    description:
+      "Gneiss is a foliated metamorphic rock identified by its bands and lenses of varying mineral composition.",
+  },
+  6: {
+    id: "6",
+    name: "Quartzite",
+    type: "Metamorphic",
+    img: "https://geology.com/rocks/pictures/quartzite.jpg",
+    description:
+      "Quartzite is a nonfoliated metamorphic rock composed almost entirely of quartz. ",
+  },
+  7: {
+    id: "7",
+    name: "Limestone",
+    type: "Sedimentary",
+    img: "https://geology.com/rocks/pictures/black-limestone-380.jpg",
+    description:
+      "Limestone is a sedimentary rock composed primarily of calcite, a calcium carbonate mineral with a chemical composition of CaCO3.",
+  },
+  8: {
+    id: "8",
+    name: "Sandstone",
+    type: "Sedimentary",
+    img: "https://geology.com/rocks/pictures/sandstone.jpg",
+    description:
+      "Sandstone is a sedimentary rock composed of sand-size grains of mineral, rock, or organic material.",
+  },
+  9: {
+    id: "9",
+    name: "Conglomerate",
+    type: "Sedimentary",
+    img: "https://geology.com/rocks/pictures/conglomerate-380.jpg",
+    description:
+      "Conglomerate is a clastic sedimentary rock made up of rounded clasts that are greater than two millimeters in diameter.",
+  },
+};
+
+export default {
+  rocks,
+};
