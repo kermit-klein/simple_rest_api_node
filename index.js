b306ac8bd12955a89dae8d85497ab30e0a3b8e2d
Ali Erbay <alierbay@Alis-MacBook-Pro.local>
2021-05-04 08:49:53 +0200

    Add comment

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Using middleware to pass models to routes by attaching them to request object
 app.use((req, res, next) => {
   req.db = {
     models,
